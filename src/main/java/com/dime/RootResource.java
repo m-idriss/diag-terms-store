390501a64dd8c2080218573e5480de9ee0d7e13a
Idriss MOHAMADY <pro.mohamady@gmail.com>
2025-01-05 16:29:12 +0100

    Update swagger path and redirect root page to it

--- src/main/java/com/dime/RootResource.java
+++ src/main/java/com/dime/RootResource.java
@@ -3,16 +3,20 @@ package com.dime;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
 @Path("/")
 public class RootResource {
 
+  @ConfigProperty(name = "quarkus.swagger-ui.path")
+  String swaggerPath;
+
   @GET
   @Operation(hidden = true) // This hides the endpoint from the Swagger UI
   public Response redirectToSwagger() {
     return Response.status(Response.Status.FOUND)
-        .location(java.net.URI.create("/swagger-ui"))
+        .location(java.net.URI.create(swaggerPath))
         .build();
   }
 }
